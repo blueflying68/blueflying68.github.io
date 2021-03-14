b5701964d4105145906d688bda3b40fb0ffc1bc9
Ellis Lo <blueflying68@gmail.com>
2021-03-14 16:28:55 +0800

    change

--- config.rb
+++ config.rb
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-require 'compass/import-once/activate'
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-=======
 Encoding.default_external = 'utf-8'
 require 'susy'
 require 'breakpoint'
@@ -18,23 +10,10 @@ require 'compass/import-once/activate'
 
 http_path = "/"
 css_dir = ""
->>>>>>> 6315e33 (modify)
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-<<<<<<< HEAD
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-preferred_syntax = :scss
-=======
 output_style = :expanded
 sourcemap = false
 
@@ -57,4 +36,3 @@ sass_options = {:debug_info=>false}
 Sass::Script::Number.precision=8
 
 
->>>>>>> 6315e33 (modify)
